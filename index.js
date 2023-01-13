467ce4b70d9d2887bb7ef44721bf413115cbc398
shivg90 <siobhan.gatenby1@live.co.uk>
2023-01-13 11:43:08 +0100

    commented old code

--- index.js
+++ index.js
@@ -81,3 +81,15 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
   });
+
+
+
+/* OLD CODE */
+// const http = require('http');
+
+// http.createServer((request, response) => {
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+//   response.end('Welcome to my book club!\n');
+// }).listen(8080);
+
+// console.log('My first Node test server is running on Port 8080.');
