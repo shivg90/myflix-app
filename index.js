585273404e102ad86d4c032e6975e6dec1f7c3ce
shivg90 <siobhan.gatenby1@live.co.uk>
2023-01-13 11:43:29 +0100

    removed practice code

--- index.js
+++ index.js
@@ -5,18 +5,12 @@ const express = require('express');
 /* declaring the variable 'app' and attached all functionalities of express to it */
 const app = express();
 
-const http = require('http');
 /* invoking morgan, instead of myLogger() function */
 app.use(morgan('common'));
 
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Welcome to my book club!\n');
-}).listen(8080);
 /* allows the return of multiple static files in response to a request */
 app.use(express.static('public'));
 
-console.log('My first Node test server is running on Port 8080.');    {
 let myMovies = [
     {
       title: '2001: A Space Odyssey',
