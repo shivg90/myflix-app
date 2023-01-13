0c0bfa740d13d6a96ce1fb151bb6fa0363c3556f
shivg90 <siobhan.gatenby1@live.co.uk>
2023-01-13 11:39:33 +0100

    defined array in myMovies object

--- index.js
+++ index.js
@@ -10,4 +10,46 @@ http.createServer((request, response) => {
   response.end('Welcome to my book club!\n');
 }).listen(8080);
 
-console.log('My first Node test server is running on Port 8080.');
+console.log('My first Node test server is running on Port 8080.');    {
+      title: '2001: A Space Odyssey',
+      release: '1968'
+    },
+    {
+      title: 'The Godfather',
+      release: '1972'
+    },
+    {
+      title: 'Citizen Kane',
+      release: '1941'
+    },
+    {
+      title: 'Raiders of the Lost Ark',
+      release: '1981'
+    },
+    {
+      title: 'La Dolce Vita',
+      release: '1960'
+    },
+    {
+      title: 'Seven Samuri',
+      release: '1954'
+    },
+    {
+      title: 'In the Mood for Love',
+      release: '2000'
+    },
+    { 
+      title: 'There Will Be Blood',
+      release: '2007'
+    },
+    {
+      title: 'Singin\' in the Rain',
+      release: '1952'
+    },
+    {
+      title: 'High Fidelity',
+      release: '2000'
+    }
+
+  ];
+
