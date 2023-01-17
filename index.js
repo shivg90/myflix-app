edad8574bc460935388ac9d0b6c8b806b8ad9304
shivg90 <siobhan.gatenby1@live.co.uk>
2023-01-17 13:54:38 +0100

    added express code ex:2.5

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-/* importing the express package locally */
+/* importing all needed packages locally */
 const express = require('express');
-/* importing morgan */
-const morgan = require('morgan');
-/* declaring the variable 'app' and attached all functionalities of express to it */
-const app = express();
+      morgan = require('morgan');
+      app = express();
+      fs = require('fs');
+      path = require ('path');
+      bodyParser = require('body-parser');
+      uuid = require('uuid');
 
-const fs = require('fs');
-const path = require ('path');
-
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+const accessLogStream = fs.createWriteStream(path.join(__dirname, './log.txt.log'), {flags: 'a'})
 
 /* invoking morgan, instead of myLogger() function */
 app.use(morgan('common', {
@@ -18,60 +17,284 @@ app.use(morgan('dev'));
 
 /* allows the return of multiple static files in response to a request */
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
-let myMovies = [
+let users = [
+  {
+    id: '1.1',
+    name: 'John',
+    favouriteMovie: []
+  },
+  {
+    id: '1.2',
+    name: 'Sarah',
+    favouriteMovie: 'The Godfather'
+  }
+];
+
+let movies = [
     {
       title: '2001: A Space Odyssey',
-      release: '1968'
+      description: 'describe movie',
+      release: '1968',
+      genre: {
+        name: 'comedy',
+      }
+      director: {
+        name: 'placeholder',
+      }
+      imageUrl: 'link to image URL',
+      featured: 'false'
     },
     {
       title: 'The Godfather',
-      release: '1972'
+      description: 'describe movie',
+      release: '1972',
+      genre: {
+        name: 'thriller',
+      }
+      director: {
+        name: 'placeholder',
+      }
+      imageUrl: 'link to image URL',
+      featured: 'false'
     },
     {
       title: 'Citizen Kane',
-      release: '1941'
+      description: 'describe movie',
+      release: '1941',
+      genre: {
+        name: 'comedy',
+      }
+      director: { 
+        name: 'placeholder',
+      }
+      imageUrl: 'link to image URL',
+      featured: 'false'
     },
     {
       title: 'Raiders of the Lost Ark',
-      release: '1981'
+      description: 'describe movie',
+      release: '1981',
+      genre: {
+        name: 'thriller',
+      }
+      director: {
+        name: 'placeholder',
+      }
+      imageUrl: 'link to image URL',
+      featured: 'false'
     },
     {
       title: 'La Dolce Vita',
-      release: '1960'
+      description: 'describe movie',
+      release: '1960',
+      genre: {
+        name: 'comedy',
+      }
+      director: {
+        name: 'placeholder',
+      }
+      imageUrl: 'link to image URL',
+      featured: 'false'
     },
     {
       title: 'Seven Samuri',
-      release: '1954'
+      description: 'describe movie',
+      release: '1954',
+      genre: {
+        name: 'thriller',
+      }
+      director: {
+        name: 'placeholder',
+      }
+      imageUrl: 'link to image URL',
+      featured: 'false'
     },
     {
       title: 'In the Mood for Love',
-      release: '2000'
+      description: 'describe movie',
+      release: '2000',
+      genre: {
+        name: 'comedy',
+      }
+      director: {
+        name: 'placeholder',
+      }
+      imageUrl: 'link to image URL',
+      featured: 'false'
     },
     { 
       title: 'There Will Be Blood',
-      release: '2007'
+      description: 'describe movie',
+      release: '2007',
+      genre: {
+        name: 'thriller',
+      }
+      director: {
+        name: 'placeholder',
+      }
+      imageUrl: 'link to image URL',
+      featured: 'false'
     },
     {
       title: 'Singin\' in the Rain',
-      release: '1952'
+      description: 'describe movie',
+      release: '1952',
+      genre: {
+        name: 'comedy',
+      }
+      director: {
+        name: 'placeholder',
+      }
+      imageUrl: 'link to image URL',
+      featured: 'false'
     },
     {
       title: 'High Fidelity',
-      release: '2000'
+      description: 'describe movie',
+      release: '2000',
+      genre: {
+        name: 'thriller',
+      }
+      director: {
+        name: 'placeholder',
+      }
+      imageUrl: 'link to image URL',
+      featured: 'false'
     }
 
   ];
 
-/* GET requests */
-app.get('/movies', (req, res) => {
-    res.json(myMovies);
-  });
-
 /* res.send object replaces response.writeHead and response.end code */
 app.get('/', (req, res) => {
-    res.send('Welcome to MyFlix Movie App!');
+  res.send('Welcome to MyFlix Movie App!');
 });
+
+/* 1. GETs list of all movies */
+app.get('/movies', (req, res) => {
+    res.status(200).json(movies);
+  });
+
+/* 2. GETs specific movie data by title */
+app.get('/movies/:title', (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find( movie => movie.title === title );
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send('this movie doesn\'t exist!')
+  }
+})
+
+/* 3. GETs specific movie data by genre */
+app.get('/movies/genre/:genreName', (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find( movie => movie.genre.Name === genreName ).genre;
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send('this genre doesn\'t exist!')
+  }
+})
+
+/* 4. GETs specific movie data by director */
+app.get('/movies/director/:directorName', (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find( movie => movie.director.Name === directorName ).director;
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send('this director doesn\'t exist!')
+  }
+})
+
+/* 5. POST: allows new users to register  */
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send('user name required');
+  }
+})
+
+/* 6. PUT: allows user to update their userinfo */
+
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updateUser = req.body;
+  /* only use 2 = rather than 3 = as user.id is a number but id is  a string */
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.name = updateUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send('user name not found')
+
+  }
+})
+
+/* 7. POST: allow users to add a movie to their favourites   */
+
+app.post('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+  
+  /* only use 2 = rather than 3 = as user.id is a number but id is  a string */
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.favouriteMovie.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send('user name not found')
+
+  }
+})
+
+/* 8. DELETE: allow users to remove a movie from their favourites */
+
+app.delete('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+  
+  /* only use 2 = rather than 3 = as user.id is a number but id is  a string */
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle )
+    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
+  } else {
+    res.status(400).send('user name not found')
+
+  }
+})
+
+/* 9. DELETE: allow users to de-register */
+
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  
+  /* only use 2 = rather than 3 = as user.id is a number but id is  a string */
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    users = users.filter( user => user.id != id )
+    res.status(200).send(` user ${id} has been successfully deleted `);
+  } else {
+    res.status(400).send('user name not found')
+
+  }
+})
+
+
+
 
 /* error handler comes after all route calls and app.use but before app.listen */
 app.use((err, req, res, next) => {
@@ -85,13 +308,3 @@ app.listen(8080, () => {
   });
 
 
-
-/* OLD CODE */
-// const http = require('http');
-
-// http.createServer((request, response) => {
-//   response.writeHead(200, {'Content-Type': 'text/plain'});
-//   response.end('Welcome to my book club!\n');
-// }).listen(8080);
-
-// console.log('My first Node test server is running on Port 8080.');
