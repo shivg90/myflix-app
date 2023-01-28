1b6356d2594b5f698d80dec54e3bc795669d97f3
shivg90 <siobhan.gatenby1@live.co.uk>
2023-01-28 19:16:23 +0100

    added user validation code

--- passport.js
+++ passport.js
@@ -22,6 +22,12 @@ passport.use(new LocalStrategy(
         console.log('incorrect username');
         return callback(null, false, {message: 'Incorrect username or password.'});
       }
+
+      if (!user.validatePassword(password)) {
+        console.log('incorrect password');
+        return callback(null, false, {message: 'Incorrect password.'});
+      }
+      
       console.log('finished');
       return callback(null, user);
     });
