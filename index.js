033538930cf7d3b0373f8dacb4584c55f1db3270
shivg90 <siobhan.gatenby1@live.co.uk>
2023-02-09 11:54:06 +0100

    test/ regex and parse code

--- index.js
+++ index.js
@@ -167,6 +167,21 @@ app.post('/users',
     check('Email', 'Email does not appear to be valid').isEmail(),
     check('Birthday', 'Birthday should be in the format DD/MM/YYYY').isDate({format:'DD/MM/YYYY'})
   ], (req, res) => {
+
+    if (window.JSON && !window.JSON.dateParser) {
+      var reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
+     
+      JSON.dateParser = function (key, value) {
+          if (typeof value === 'string') {
+              var a = reISO.exec(value);
+              if (a)
+                  return new Date(value);
+          }
+          return value;
+      };
+  
+  }
+
     let errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
