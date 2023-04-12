dd1f436fc911534ba8074afb3f13974ed0f723f0
shivg90 <siobhan.gatenby1@live.co.uk>
2023-04-12 17:24:13 +0200

    added port 4200 to cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'https://movieapi-9rx2.onrender.com/', 'http://localhost:1234', 'http://localhost:56971', 'https://shivg90-myflix-movie-app.netlify.app'];
+let allowedOrigins = ['http://localhost:8080', 'https://movieapi-9rx2.onrender.com/', 'http://localhost:1234', 'http://localhost:56971', 'https://shivg90-myflix-movie-app.netlify.app', 'http://localhost:4200'];
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
